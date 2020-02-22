76dcc08dda6162721755832a1380c866d9506338
Anson12666 <Anson.12666@gmail.com>
2020-02-22 22:23:23 +0800

    22_2_2020(000webhost)

--- webpage/Opinion/Opinion.php
+++ webpage/Opinion/Opinion.php
@@ -1,15 +1,52 @@
+
 <?php
-$name = $_POST['Name']; 
-$where_know_website = $_POST['Known']; 
-$radio = $_POST['Rank']; 
-$Other_Opinion = $_POST['Opinion']; 
 
+$host = "localhost";
+$userName = "id12674694_ictsba";
+$password = "hYvbux-vawkip-9jykwy";
+$dbName = "id12674694_opinion";
+// Create database connection
+$conn = new mysqli($host, $userName, $password, $dbName);
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
 
-echo "
-你的姓名是： $Name
-職業： $Name
-你從哪裏得識此網站: $Known
-網頁評分： $Rank
-其他意見： $Opinion
-";
+$Name = $conn->real_escape_string($_POST['Name']);
+$Age = $conn->real_escape_string($_POST['Age']);
+$Known = $conn->real_escape_string($_POST['Known']);
+$Rank = $conn->real_escape_string($_POST['Rank']);
+$Opinion = $conn->real_escape_string($_POST['Opinion']);
+$Submit = $conn->real_escape_string($_POST['Submit']);
+
+$sql="INSERT INTO `Opinion` (`Name`, `Age`, `Known`, `Rank`, `Opinion`, `Submit`) VALUES ('".$Name."','".$Age."','".$Known."','".$Rank."','".$Opinion."','".$Submit."')";
+
+if(!$result = $conn->query($sql)){
+die('SQL server Error [' . $conn->error . ']');
+}
+else
+{
+	echo "
+		<h1>Success</h1>
+	";
+}
 ?>
+
+	<body style="text-align:center">
+	
+		<table align="center" width="50%" border="10" style="font-size:30px">
+		  <tr align="center">
+		    <td>本表格將於SBA報告完成后刪除</td>
+	      </tr>
+		  <tr>
+		    <td>SQL hostname:databases.000webhost.com <br />
+		      Debug website: <a href="https://databases.000webhost.com/sql.php?db=id12674694_opinion&table=Opinion&pos=0" target="new">Direct Link</a> <br />
+		      Username: id12674694_ictsba<br />
+		      Password: hYvbux-vawkip-9jykwy<br />
+		      <a style="font-size:16px">For copy and paste:
+		      id12674694_ictsba hYvbux-vawkip-9jykwy</a><br />
+		      Database Name: id12674694_opinion<br />
+	        Table: Opinion</td>
+	      </tr>
+    </table>
+	</body>
